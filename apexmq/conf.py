6aab6ddcb5326c56cc2174f4665518338fcdf3e6
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-09-14 03:29:12 +0930

    updated conf.py get_connection_params function

--- apexmq/conf.py
+++ apexmq/conf.py
@@ -13,17 +13,9 @@ def get_apexmq_settings() -> dict:
             "APEXMQ_SETTINGS is not defined in your settings.py file."
         )
 
-    # Validate the structure of the settings
-    if "connections" not in user_settings or not isinstance(
-        user_settings["connections"], dict
-    ):
+    if len(user_settings) == 0:
         raise ImproperlyConfigured(
-            "APEXMQ_SETTINGS must contain a 'connections' dictionary."
-        )
-
-    if "queues" not in user_settings or not isinstance(user_settings["queues"], dict):
-        raise ImproperlyConfigured(
-            "APEXMQ_SETTINGS must contain a 'queues' dictionary."
+            "Cannot find any host connections defined in APEXMQ_SETTINGS."
         )
 
     return user_settings
@@ -37,7 +29,14 @@ def get_connection_params(connection_name) -> dict:
 
     if connection_name not in settings:
         raise ImproperlyConfigured(
-            f"apexmq connection '{connection_name}' is not defined in APEXMQ_SETTINGS."
+            f"'{connection_name}' is not defined in APEXMQ_SETTINGS."
         )
 
-    return settings[connection_name]
+    connection_settings = settings[connection_name]
+
+    if "user" not in connection_settings or "password" not in connection_settings:
+        raise ImproperlyConfigured(
+            f"Server logging data has not defined in '{connection_name}' configurations."
+        )
+
+    return connection_settings
