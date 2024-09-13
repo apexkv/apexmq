84b9c9e81d868b3c682fc8d8825153f348c25c4b
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-09-14 04:22:28 +0930

    created create_que method

--- apexmq/connection.py
+++ apexmq/connection.py
@@ -2,28 +2,49 @@ import pika
 from typing import Dict
 from django.conf import settings
 from .conf import get_connection_params
+from pika.adapters.blocking_connection import BlockingChannel
 
 
 class ApexMQQueManager:
-    _que_list = {}
+    _que_list: Dict[str, "ApexMQQueManager"] = {}
 
-    def __init__(self, channel, que_name):
+    def __init__(self, channel: BlockingChannel, que_name):
         self.channel = channel
         self.que_name = que_name
-        self.que = None
+        self.que = channel.queue_declare(queue=que_name)
+        self._que_list[self.que_name] = self
 
 
 class ApexMQChannelManager:
-    _channel_list = {}
+    _channel_list: Dict[str, "ApexMQChannelManager"] = {}
 
     def __init__(self, connection: pika.BlockingConnection, channel_name):
         self.connection = connection
         self.channel_name = channel_name
         self.channel = connection.channel()
+        self.que_list: Dict[str, ApexMQQueManager] = {}
+        self._channel_list[self.channel_name] = self
+
+    def create_que(self, que_name):
+        """
+        Create a new queue with the given name and add it to the queue list.
+
+        This method creates a new ApexMQQueManager instance, which in turn
+        declares a new queue on the RabbitMQ server.
+
+        Args:
+            que_name (str): The name of the queue to create.
+
+        Returns:
+            ApexMQQueManager: A new queue manager instance.
+        """
+        new_que = ApexMQQueManager(self.channel, que_name)
+        self.que_list[que_name] = new_que
+        return new_que
 
 
 class ApexMQConnectionManager:
-    _connection_list: Dict[str, pika.BlockingConnection] = {}
+    _connection_list: Dict[str, "ApexMQConnectionManager"] = {}
 
     def __init__(self, connection_name):
         self.connection_name = connection_name
@@ -50,7 +71,7 @@ class ApexMQConnectionManager:
         except Exception as e:
             raise ConnectionError(f"Failed to connect to messege que server: {e}")
 
-        self._connection_list[self.connection_name] = self.connection
+        self._connection_list[self.connection_name] = self
 
         return self.connection
 
