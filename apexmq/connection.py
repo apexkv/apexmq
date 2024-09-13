09282b4ff00ffd198ade4bd66a9138414405b3c7
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-09-14 02:28:02 +0930

    create ApexMQQueManager class

--- apexmq/connection.py
+++ apexmq/connection.py
@@ -1,1 +1,11 @@
+import pika
+from django.conf import settings
 
+
+class ApexMQQueManager:
+    _que_list = {}
+
+    def __init__(self, channel, que_name):
+        self.channel = channel
+        self.que_name = que_name
+        self.que = None
