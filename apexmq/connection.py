9e5999f0d18aeb38a5569aafd75b7129ae40690e
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-09-14 02:44:09 +0930

    created ApexMQConnectionManager class

--- apexmq/connection.py
+++ apexmq/connection.py
@@ -1,5 +1,6 @@
 import pika
 from django.conf import settings
+from .conf import get_connection_params
 
 
 class ApexMQQueManager:
@@ -18,3 +19,13 @@ class ApexMQChannelManager:
         self.connection = connection
         self.channel_name = channel_name
         self.channel = None
+
+
+class ApexMQConnectionManager:
+    _connection_list = {}
+
+    def __init__(self, connection_name):
+        self.connection_name = connection_name
+        self.connection_params = get_connection_params(connection_name)
+        self.connection = None
+        self.channel_list = {}
