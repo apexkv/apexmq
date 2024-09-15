613eee585d273890ed154e2069c886c639104315
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-09-15 20:41:34 +0930

    created setup_rabbitmq and stop_threads methods

--- apexmq/apps.py
+++ apexmq/apps.py
@@ -1,3 +1,4 @@
+import threading
 from django.apps import AppConfig
 from django.core.exceptions import ImproperlyConfigured
 
@@ -15,6 +16,60 @@ class ApexMQConfig(AppConfig):
 
     def ready(self):
         pass
+
+    def setup_rabbitmq(self, sender, **kwargs):
+        """
+        Sets up RabbitMQ connections and channels. This method is called
+        when Django detects a code change in DEBUG mode.
+        """
+        global thread_list
+        # Stop any existing RabbitMQ consumer threads
+        self.stop_threads()
+        # Fetch RabbitMQ settings
+        apexmq_settings = get_apexmq_settings()
+
+        if not apexmq_settings:
+            raise ImproperlyConfigured(
+                "RabbitMQ connection configurations are not provided."
+            )
+
+        # Iterate over all RabbitMQ connection configurations
+        for connection_name, config in apexmq_settings.items():
+            connection_manager = ApexMQConnectionManager(connection_name)
+            connection_manager.connect()
+
+            # Iterate over all channels and queues in the configuration
+            for channel_name, channel_config in config.get("CHANNELS", {}).items():
+                channel_manager = connection_manager.create_channel(channel_name)
+
+                for queue_name in channel_config.get("QUEUES", {}).keys():
+                    queue_manager = ApexMQQueueManager(
+                        channel=channel_manager.channel, queue_name=queue_name
+                    )
+
+                    # Set up message consummer for each queue
+                    channel_manager.channel.basic_consume(
+                        queue=queue_name,
+                        on_message_callback=self.message_callback,
+                        auto_ack=True,
+                    )
+
+                # Start a new thread to handle message consumption
+                thread = threading.Thread(
+                    target=channel_manager.channel.start_consuming, daemon=True
+                )
+                thread_list.append(thread)
+                thread.start()
+
+    def stop_threads(self):
+        """
+        Stops all running consumer threads by joining them.
+        """
+        global thread_list
+        for thread in thread_list:
+            if thread.is_alive():
+                thread.join(timeout=1)
+        thread_list = []
 
     def message_callback(self, channel, method, properties, body):
         """
