640a6dbf6f0b783d6be000badc010cb63476cd50
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-09-18 18:29:27 +0930

    Refactor producers.py to use ApexMQChannelManager's publish method
    Instead of directly accessing the channel and properties in producers.py, refactor the code to use the publish method provided by ApexMQChannelManager. This improves code organization and encapsulation, making it easier to manage and maintain the messaging functionality.

--- apexmq/producers.py
+++ apexmq/producers.py
@@ -14,19 +14,9 @@ def producer(
     to: List[str],
     channel_name=ApexMQChannelManager.get_first_channel_name(),
 ):
-    channel = ApexMQChannelManager.get_channel(channel_name).channel
-    properties = pika.BasicProperties(action)
+    channel_manager = ApexMQChannelManager.get_channel(channel_name)
     for publish_to in to:
         try:
-            # Ensure the queue exists (even if declared by other microservices)
-            channel.queue_declare(queue=publish_to)
-
-            # Publish the message to the queue
-            channel.basic_publish(
-                exchange="",
-                routing_key=publish_to,  # Send directly to the queue
-                body=json.dumps(body),
-                properties=properties,
-            )
+            channel_manager.publish(action, body, publish_to)
         except Exception as e:
             logger.error(f"Failed to publish message to {publish_to}: {e}")
