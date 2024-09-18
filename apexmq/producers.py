bfd26a2b178d3c92f542011e5c2a6d0fb4e698ea
kavindu harshitha <kavindu.harshitha97@gmail.com>
2024-09-18 18:39:00 +0930

    Refactor producers.py to use ApexMQChannelManager's publish method

--- apexmq/producers.py
+++ apexmq/producers.py
@@ -2,6 +2,8 @@ import json
 import logging
 import pika
 from typing import List
+
+from .conf import get_first_channel_name
 from .connection import ApexMQChannelManager
 
 logger = logging.getLogger(__name__)
@@ -12,7 +14,7 @@ def producer(
     action: str,
     body: dict,
     to: List[str],
-    channel_name=ApexMQChannelManager.get_first_channel_name(),
+    channel_name=get_first_channel_name(),
 ):
     channel_manager = ApexMQChannelManager.get_channel(channel_name)
     for publish_to in to:
