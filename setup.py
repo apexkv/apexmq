59d9cbcf224f1d2c69d7faebbaf1577137b123af
Kavindu Harshitha <kavindu@apexkv.com>
2024-09-22 07:37:59 +0930

    updated setup.py file and created release workflow

--- setup.py
+++ setup.py
@@ -13,12 +13,14 @@ setup(
     url="https://github.com/apexkv/apexmq",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Framework :: Django",
         "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
     ],
     packages=find_packages(),
     install_requires=[
         "pika>=1.1.0",
+        "django>=3.0.0",
     ],
     python_requires=">=3.6",
 )
