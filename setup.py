ddf0d8fe83a03171fa0e966a5c8382a6512b72dd
Kavindu Harshitha <kavindu@apexkv.com>
2024-09-26 09:23:54 +0930

    updated git version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="apexmq",
-    version="1.0.0",
+    version="1.0.1",
     description="A developer-friendly library for integrating RabbitMQ with Django applications.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
