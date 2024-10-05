c6b4fee590a0c519e80f6ce4dd4ff65f7d1a96d3
Kavindu Harshitha <kavindu@apexkv.com>
2024-10-05 17:32:25 +0930

    Bump version to 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="apexmq",
-    version="1.0.5",
+    version="1.0.6",
     description="A developer-friendly library for integrating RabbitMQ with Django applications.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
