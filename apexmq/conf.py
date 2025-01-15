9b42ec83731f7f6b4176ff37a550ee85fbe64b9b
Kavindu Harshitha <kavindu.harshitha97@gmail.com>
2025-01-15 10:51:02 +1030

    Add configuration and logging enhancements to apexmq
    - Introduced Pydantic models for queue and connection settings.
    - Added utility functions for simplifying dictionary keys and fetching project name.
    - Refactored connection settings retrieval to use Pydantic for validation.
    - Implemented a Logger class for structured logging with timestamp formatting.

--- apexmq/conf.py
+++ apexmq/conf.py
@@ -1,12 +1,50 @@
-import importlib
-import inspect
 import logging
+from typing import Dict
+from pydantic import BaseModel
 from django.utils import timezone
 from django.conf import settings
-from django.apps import apps
 from django.core.exceptions import ImproperlyConfigured
 
-from .consumers import BaseConsumer
+
+def get_project_name()->str:
+    """
+    Fetches the name of the Django project from the ROOT_URLCONF setting.
+    """
+    return settings.ROOT_URLCONF.split(".")[0].lower()
+
+
+def simplify_keys(d):
+    """Recursively simplifies keys of a dictionary."""
+    if isinstance(d, dict):
+        return {key.lower(): simplify_keys(value) for key, value in d.items()}
+    elif isinstance(d, list):
+        return [simplify_keys(item) for item in d]
+    else:
+        return d
+
+
+DEFAULT_QUEUE_NAME = get_project_name()
+APP_NAME = "apexmq"
+
+
+class ApexMQSettingsQueue(BaseModel):
+    durable: bool = True
+    exclusive: bool = False
+    passive: bool = False
+    auto_delete: bool = False
+    auto_ack: bool = False
+
+
+class ApexMQSettingsConnection(BaseModel):
+    user: str
+    password: str
+    host: str = "localhost"
+    port: int = 5672
+    vhost: str = "/"
+    queue: Dict[str, ApexMQSettingsQueue] = {
+        DEFAULT_QUEUE_NAME: ApexMQSettingsQueue()
+    }
+    retries: int = 5
 
 
 def get_apexmq_settings() -> dict:
@@ -19,23 +57,24 @@ def get_apexmq_settings() -> dict:
     Raises:
         ImproperlyConfigured: If APEXMQ_SETTINGS is not defined or is empty in settings.
     """
+    APEXMQ_SETTINGS = "APEXMQ_SETTINGS"
     # Retrieve the APEXMQ_SETTINGS from Django settings
-    user_settings = getattr(settings, "APEXMQ_SETTINGS", None)
+    user_settings = getattr(settings, APEXMQ_SETTINGS, None)
 
     if not user_settings:
         raise ImproperlyConfigured(
-            "APEXMQ_SETTINGS is not defined in your settings.py file."
+            f"{APEXMQ_SETTINGS} is not defined in your settings.py file."
         )
 
     if len(user_settings) == 0:
         raise ImproperlyConfigured(
-            "Cannot find any host connections defined in APEXMQ_SETTINGS."
+            f"Cannot find any host connections defined in {APEXMQ_SETTINGS}."
         )
 
-    return user_settings
+    return simplify_keys(user_settings)
 
 
-def get_connection_settings() -> dict:
+def get_connection_settings() -> ApexMQSettingsConnection:
     """
     Retrieve the apexmq connection settings from the APEXMQ_SETTINGS dictionary.
 
@@ -45,104 +84,37 @@ def get_connection_settings() -> dict:
     Raises:
         ImproperlyConfigured: If "CONNECTION" not in APEXMQ_SETTINGS.
     """
-    KEY = "CONNECTION"
-    # Fetch APEXMQ settings
+    CONNECTION = "connection"
     settings = get_apexmq_settings()
 
-    if KEY not in settings:
-        raise ImproperlyConfigured(f"{KEY} is not defined in APEXMQ_SETTINGS.")
+    if CONNECTION not in settings:
+        raise ImproperlyConfigured(f"{CONNECTION} is not defined in APEXMQ_SETTINGS.")
 
-    return settings[KEY]
+    connection = ApexMQSettingsConnection(**settings[CONNECTION])
 
-
-def get_connection_params(connection_name) -> dict:
-    """
-    Retrieve the apexmq connection parameters for the specified connection name.
-
-    Args:
-        connection_name (str): The name of the connection.
-
-    Returns:
-        dict: The connection parameters for the specified connection name.
-
-    Raises:
-        ImproperlyConfigured: If the connection name is not found or required fields are missing.
-    """
-    # Fetch APEXMQ settings
-    settings = get_connection_settings()
-
-    if connection_name not in settings:
-        raise ImproperlyConfigured(
-            f"'{connection_name}' is not defined in APEXMQ_SETTINGS."
-        )
-
-    connection_settings = settings[connection_name]
-
-    # Ensure that required fields are present in the connection settings
-    if "USER" not in connection_settings or "PASSWORD" not in connection_settings:
-        raise ImproperlyConfigured(
-            f"Server logging data has not defined in '{connection_name}' configurations."
-        )
-
-    return connection_settings
+    return connection
 
 
-def get_consumers_from_apps():
-    """
-    Retrieves all consumer classes that are subclasses of BaseConsumer from the consumers.py files of installed apps.
+class Logger:
+    logger = logging.getLogger(__name__)
 
-    Returns:
-        list: A list of the consumer classes.
+    def log(self, msg):
+        timestamp = timezone.now()
+        details = f"[{timestamp.day:02d}/{timestamp.month:02d}/{timestamp.year} {timestamp.hour:02d}:{timestamp.minute:02d}:{timestamp.second:02d}] {msg}"
+        return details
+    
+    @classmethod
+    def info(cls, msg):
+        details = cls().log(msg)
+        cls.logger.info(details)
+        print(details)
+    
+    @classmethod
+    def warning(cls, msg):
+        details = cls().log(msg)
+        cls.logger.warning(details)
 
-    Notes:
-        - Iterates over all installed apps and attempts to import their consumers.py module.
-        - If the module contains classes that are subclasses of BaseConsumer, they are added to the result.
-    """
-    consumers_dict = []
-    FILE_NAME = "consumers"
-
-    # Iterate over all installed apps
-    for app_config in apps.get_app_configs():
-        if app_config.name != "apexmq":
-            try:
-                # Construct the path to the consumer.py file in the app
-                module_path = f"{app_config.name}.{FILE_NAME}"
-
-                # Dynamically import the consumer.py module
-                module = importlib.import_module(module_path)
-
-                class_list = inspect.getmembers(module, inspect.isclass)
-
-                for name, classobject in class_list:
-                    if issubclass(classobject, BaseConsumer):
-                        consumers_dict.append(classobject)
-
-            except ModuleNotFoundError:
-                # Skip if the app doesn't have a consumers.py file
-                continue
-            except Exception as e:
-                continue
-
-    return consumers_dict
-
-
-logger = logging.getLogger(__name__)
-
-
-def info(msg):
-    timestamp = timezone.now()
-    details = f"[{timestamp.day:02d}/{timestamp.month:02d}/{timestamp.year} {timestamp.hour:02d}:{timestamp.minute:02d}:{timestamp.second:02d}] {msg}"
-    logger.info(details)
-    print(details)
-
-
-def warning(msg):
-    timestamp = timezone.now()
-    details = f"[{timestamp.day:02d}/{timestamp.month:02d}/{timestamp.year} {timestamp.hour:02d}:{timestamp.minute:02d}:{timestamp.second:02d}] {msg}"
-    logger.warning(details)
-
-
-def error(msg):
-    timestamp = timezone.now()
-    details = f"[{timestamp.day:02d}/{timestamp.month:02d}/{timestamp.year} {timestamp.hour:02d}:{timestamp.minute:02d}:{timestamp.second:02d}] {msg}"
-    logger.error(details)
+    @classmethod
+    def error(cls, msg):
+        details = cls().log(msg)
+        cls.logger.error(details)
