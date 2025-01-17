227d5da47754de03ccdf6b4742665a4883980186
Kavindu Harshitha <kavindu.harshitha97@gmail.com>
2025-01-17 11:42:52 +1030

    implemented set_credentials method

--- apexmq/managers/connection.py
+++ apexmq/managers/connection.py
@@ -21,6 +21,25 @@ class ApexMQConnectionManager:
     def __init__(self):
         self.connection:BlockingConnection|None = None
         self.params = get_connection_settings()
+        self.credentials:pika.PlainCredentials|None = None
+
+    def set_credentials(self):
+        """
+        Sets the credentials to authenticate with RabbitMQ.
+
+        Notes:
+            - The credentials are set using the PlainCredentials class from the pika library.
+        """
+        if not self.params:
+            raise ImproperlyConfigured("RabbitMQ settings are required.")
+
+        if not self.params.user or not self.params.password:
+            raise ImproperlyConfigured("RabbitMQ user and password are required.")
+        
+        self.credentials = pika.PlainCredentials(
+            self.params.user,
+            self.params.password,
+        )
 
     def connect(self):
         """
@@ -38,10 +57,7 @@ class ApexMQConnectionManager:
             Logger.info("Connection to RabbitMQ already established.")
             return
         
-        self.credentials = pika.PlainCredentials(
-            self.params.user,
-            self.params.password,
-        )
+        self.set_credentials()
 
         retries = self.params.retries
         WAIT_TIME = 3
@@ -82,3 +98,5 @@ class ApexMQConnectionManager:
                 Logger.error(f"Error closing RabbitMQ connection: {e}")
             finally:
                 self.connection = None
+        else:
+            Logger.info("No connection to close.")
